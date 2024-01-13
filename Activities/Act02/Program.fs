af8230bd33dc6730a3be41631a3d2df8f00e2d6e
Sang Kil Cha <sangkilc@kaist.ac.kr>
2024-01-13 20:36:12 +0900

    Add Act03

--- Activities/Act02/Program.fs
+++ Activities/Act02/Program.fs
@@ -1,12 +1,9 @@
 ﻿/// KAIST CS220 In-Class Activity Project
 
-/// Modify the function `safeSub` in such a way that the function takes in two
-/// 32-bit integers a and b, and returns "a - b" as output.
-///   - In normal cases, it should return the resulting number.
-///   - In error cases, i.e., overflow cases, it should simply return 0. Your
-///     function should have the type: "int -> int -> int".
-let safeSub lhs rhs =
+/// Modify the function `max` in such a way that the function takes in four
+/// 32-bit integers, and returns the largest number as output.
+let max a b c d =
   0
 
-safeSub 1 42
+max -42 42 0 -1
 |> printfn "%d"
