12341494d03410965ed571c00200ad68d9318616
Sang Kil Cha <sangkilc@kaist.ac.kr>
2024-01-13 20:15:00 +0900

    Fix name

--- Activities/Act01/Program.fs
+++ Activities/Act01/Program.fs
@@ -1,9 +1,9 @@
 ﻿/// KAIST CS220 In-Class Activity Project
 
-/// Modify the function `squared` in such a way that the function takes in an
+/// Modify the function `square` in such a way that the function takes in an
 /// integer and returns the square of the integer.
-let squared n =
+let square n =
   0
 
-squared 42
+square 42
 |> printfn "%d"
