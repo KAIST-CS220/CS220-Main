0dafde1b2940403c55ce63cf0025fef3d9c76894
Sang Kil Cha <sangkilc@kaist.ac.kr>
2024-01-13 20:10:44 +0900

    Add Act02

--- Activities/Act01/Program.fs
+++ Activities/Act01/Program.fs
@@ -1,12 +1,9 @@
 ﻿/// KAIST CS220 In-Class Activity Project
 
-/// Modify the function `safeSub` in such a way that the function takes in two
-/// 32-bit integers a and b, and returns "a - b" as output.
-///   - In normal cases, it should return the resulting number.
-///   - In error cases, i.e., overflow cases, it should simply return 0. Your
-///     function should have the type: "int -> int -> int".
-let safeSub lhs rhs =
+/// Modify the function `squared` in such a way that the function takes in an
+/// integer and returns the square of the integer.
+let squared n =
   0
 
-safeSub 1 42
+squared 42
 |> printfn "%d"
