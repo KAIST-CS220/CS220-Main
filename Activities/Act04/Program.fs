5a92a63a56e81a0966ded4f43a13e6cdc14c6aa2
Sang Kil Cha <sangkilc@kaist.ac.kr>
2024-01-21 10:05:59 +0900

    Add Act04 and Act05

---
+++ Activities/Act04/Program.fs
@@ -0,0 +1,15 @@
+﻿/// KAIST CS220 In-Class Activity Project
+
+type RationalNumber = float * float
+
+let numer (n: RationalNumber) = fst n
+
+let denom (n: RationalNumber) = snd n
+
+/// Fix this function so that the rational numbers are comparable.
+let makeRat n d = (n, d)
+
+let r0 = makeRat 1 (-2)
+let r1 = makeRat (-1) 2
+r0 = r1
+|> printfn "Is r0 and r1 the same? => %b"
---
+++ Activities/Act05/Program.fs
@@ -0,0 +1,20 @@
+﻿/// KAIST CS220 In-Class Activity Project
+
+type Shape =
+  /// A circle of a radius.
+  | Circle of float
+  /// A square with a side length.
+  | Square of float
+  /// A triangle with side lengths.
+  | Triangle of float * float * float
+
+let area shape =
+  0.0
+
+let circle = Circle 0.5
+let square = Square 1.0
+let triangle = Triangle (1.0, 1.0, 1.0)
+
+printfn "circle = %f" <| area circle
+printfn "square = %f" <| area square
+printfn "triangle = %f" <| area triangle
