8d85051d2595376f8641b8730ca5c2082f8afd8e
Sang Kil Cha <sangkilc@kaist.ac.kr>
2024-02-03 06:26:28 +0900

    Add Act15

---
+++ Activities/Act15/Program.fs
@@ -0,0 +1,14 @@
+﻿/// KAIST CS220 In-Class Activity Project
+
+module CS220.Program
+
+/// Fix this.
+let length _ = failwith "Implement"
+
+/// This is the main entry point.
+[<EntryPoint>]
+let main _ =
+  [| 4; 2 |] |> length |> printfn "%d"
+  [ 4; 2 ] |> length |> printfn "%d"
+  "abcd" |> length |> printfn "%d"
+  0 (* Never modify this *)
