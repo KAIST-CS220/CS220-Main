21829142a344316d9f1dc3517eb79e137aa3ae2b
Sang Kil Cha <sangkilc@kaist.ac.kr>
2024-02-06 14:50:32 +0900

    Add Act18

---
+++ Activities/Act18/Program.fs
@@ -0,0 +1,21 @@
+﻿/// KAIST CS220 In-Class Activity Project
+
+module CS220.Program
+
+open FSharpx.Collections
+
+let inf =
+  LazyList.unfold (fun n -> Some (n, n + 1)) 1
+
+// Problem
+let pairwise lst =
+  failwith "Implement"
+
+/// This is the main entry point.
+[<EntryPoint>]
+let main _ =
+  inf
+  |> pairwise
+  |> LazyList.take 10
+  |> LazyList.iter (fun (a, b) -> printfn "%d, %d" a b)
+  0 (* Never modify this *)
