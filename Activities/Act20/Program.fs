ccfb79640b157a804957f0d105f22d897356a8dd
Sang Kil Cha <sangkilc@kaist.ac.kr>
2024-02-07 10:56:50 +0900

    Add Act20

---
+++ Activities/Act20/Program.fs
@@ -0,0 +1,22 @@
+﻿/// KAIST CS220 In-Class Activity Project
+
+module CS220.Program
+
+type ListBuilder () =
+  member _.For (exprs, f) =
+    failwith "Implement"
+
+  member _.Yield (x) =
+    failwith "Implement"
+
+let mylist = ListBuilder ()
+
+/// This is the main entry point.
+[<EntryPoint>]
+let main _ =
+  mylist { for i in [1 .. 10] do yield i * i }
+  |> List.iter (printfn "%d")
+  printfn ""
+  mylist { for i = 10 downto 1 do yield i }
+  |> List.iter (printfn "%d")
+  0 (* Never modify this *)
